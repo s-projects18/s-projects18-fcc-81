10fa334c91dc1f84354c12135ab405e6dce1b4a7
sprojects18 <info@sgenschow.de>
2020-08-05 21:23:23 +0000

    parameter check

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 def calculate(list):
+  """list: containing 9 digits"""
+  if len(list)!=9:
+    raise ValueError("List must contain nine numbers.")
+  ar = np.array(list).reshape((3,3))
+  print(ar)
 
-
-
-
-    return calculations
+  calculations=None
+  return calculations
